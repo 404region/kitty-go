6c51009a2bb598adcd55bf8510c5fe61005e84a4
Ann S <404region@gmail.com>
2024-02-03 09:24:49 +0300

    Делаю функцию translateRomanToArabic

--- calc/main.go
+++ calc/main.go
@@ -19,6 +19,58 @@ a + b, a - b, a * b, a / b. Данные передаются в одну стр
 2) Калькулятор умеет работать как с арабскими (1, 2, 3, 4, 5…),
 так и с римскими (I, II, III, IV, V…) числами.
 */
+var cartaRomanToArabic = map[string]int{
+	"I":  1,
+	"IV": 4,
+	"V":  5,
+	"IX": 9,
+	"X":  10,
+	"XL": 40,
+	"L":  50,
+	"XC": 90,
+	"C":  100,
+}
+var cartaArabicToRoman = map[int]string{
+	1:   "I",
+	4:   "IV",
+	5:   "V",
+	9:   "IX",
+	10:  "X",
+	40:  "XL",
+	50:  "L",
+	90:  "XC",
+	100: "C",
+}
+
+type arabicToRomanStruct struct {
+	arabic int
+	roman  string
+}
+
+var arabicToRomanArr = []arabicToRomanStruct{
+	{arabic: 100, roman: "C"},
+	{arabic: 90, roman: "XC"},
+	{arabic: 50, roman: "L"},
+	{arabic: 40, roman: "XL"},
+	{arabic: 10, roman: "X"},
+	{arabic: 9, roman: "IX"},
+	{arabic: 5, roman: "V"},
+	{arabic: 4, roman: "IV"},
+	{arabic: 1, roman: "I"},
+}
+
+func translateRomanToArabic(romanNum string) (arabicNum int) {
+	for _, el := range arabicToRomanArr {
+		fmt.Println(el.roman, el.arabic)
+	}
+	/*if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+	*/
+	return 0
+}
+
 func calc(a int, b int, sign string) (result int) {
 	switch {
 	case sign == "+":
@@ -62,7 +114,7 @@ func main() {
 		fmt.Println("res[0] ", res[0][1], res[0][2], res[0][3])
 		fmt.Println("len(res[0]) ", len(res[0]))
 
-		//  считаем
+		//  конвертируем
 		num1, _ := strconv.Atoi(res[0][1])
 		num2, _ := strconv.Atoi(res[0][3])
 
@@ -70,7 +122,12 @@ func main() {
 		fmt.Println(calc(num1, num2, res[0][2]))
 	} else if len(resRoman) > 0 && len(resRoman[0]) >= 4 {
 		fmt.Println("resRoman ", resRoman) // [[I+I I + I]]
+		// конвертируем в римские
+		fmt.Println("res[0] ", resRoman[0][1], resRoman[0][2], resRoman[0][3])
+		num1 := translateRomanToArabic(resRoman[0][1])
+		// num2, _ := strconv.Atoi(res[0][3])
+		fmt.Println(num1)
 	} else {
-		// Ошибка
+		// Ошибка - вывести пользователю ошибку
 	}
 }
